014a9177a89edbd989fd51ff97ad2b970605ec16
Alexander Lind <alex.anonym@gmail.com>
2022-12-14 12:01:12 +0100

    created logic that allows delivery logs and lines to be saved to the DB

--- src/controllers/deliveryLog.js
+++ src/controllers/deliveryLog.js
@@ -1,13 +1,38 @@
 import { sendDataResponse } from '../utils/responses.js'
+import dbClient from '../utils/dbClient.js'
 
 export const create = async (req, res) => {
   const { date, cohort_id: cohortId, lines } = req.body
+  const userId = req.user.id
+
+  const createdLog = await dbClient.deliveryLog.create({
+    data: {
+      cohort: {
+        connect: { id: cohortId }
+      },
+      date: new Date(date),
+      user: {
+        connect: { id: userId }
+      }
+    }
+  })
+
+  lines.forEach(async (line) => {
+    await dbClient.deliveryLogLine.create({
+      data: {
+        content: line.content,
+        log: {
+          connect: { id: createdLog.id }
+        }
+      }
+    })
+  })
 
   return sendDataResponse(res, 201, {
     log: {
       id: 1,
       cohort_id: cohortId,
-      date,
+      date: new Date(date),
       author: {
         id: req.user.id,
         first_name: req.user.firstName,
