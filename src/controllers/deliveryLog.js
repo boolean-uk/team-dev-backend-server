23559a97e35786b6a99767c7cbdc0cbc5896d30b
Alexander Lind <alex.anonym@gmail.com>
2022-12-14 16:11:42 +0100

    updated Delivery Log schema and test for empty req body

--- src/controllers/deliveryLog.js
+++ src/controllers/deliveryLog.js
@@ -3,9 +3,12 @@ import { createLog } from '../domain/log.js'
 
 export const create = async (req, res) => {
   try {
+    if (Object.keys(req.body).length === 0) {
+      return sendMessageResponse(res, 400, 'Bad Request')
+    }
     const createdLog = await createLog(req.body, req.user)
     return sendDataResponse(res, 201, createdLog)
   } catch (error) {
-    return sendMessageResponse(res, 500, 'unable to create log')
+    return sendMessageResponse(res, 500, 'Unable to create log')
   }
 }
