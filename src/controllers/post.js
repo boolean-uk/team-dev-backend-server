879c31b35030e74d969ca9d08d65db00f531ee74
patrik <patrik.ellini@outlook.com>
2022-12-14 14:49:13 +0000

    posts are now added to the DB

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,13 +1,25 @@
-import { sendDataResponse } from '../utils/responses.js'
+import dbClient from '../utils/dbClient.js'
+import { sendDataResponse, sendMessageResponse } from '../utils/responses.js'
 
 export const create = async (req, res) => {
-  const { content } = req.body
+  const { content, userId } = req.body
 
   if (!content) {
-    return sendDataResponse(res, 400, { content: 'Must provide content' })
+    return sendMessageResponse(res, 400, { content: 'Must provide content' })
   }
 
-  return sendDataResponse(res, 201, { post: { id: 1, content } })
+  const post = await dbClient.post.create({
+    data: {
+      content,
+      user: {
+        connect: {
+          id: Number(userId)
+        }
+      }
+    }
+  })
+
+  return sendDataResponse(res, 201, post)
 }
 
 export const getAll = async (req, res) => {
