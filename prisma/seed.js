7497d0cf480086d675352c41043cfbc70f952e79
Alexander Lind <alex.anonym@gmail.com>
2022-12-16 14:05:42 +0100

    finished seed file for all models, including comments

--- prisma/seed.js
+++ prisma/seed.js
@@ -26,8 +26,6 @@ async function main() {
     }
   })
 
-  console.log(nathanTeacher)
-
   const nathanStudent = await dbClient.user.upsert({
     where: { email: 'ngk10@gmail.co' },
     update: {},
@@ -49,27 +47,102 @@ async function main() {
     }
   })
 
-  console.log(nathanStudent)
+  const nathanStudentPosts = await dbClient.post.createMany({
+    data: [
+      { userId: nathanStudent.id, content: 'Students first post!' },
+      { userId: nathanStudent.id, content: 'Students second post!' },
+      { userId: nathanStudent.id, content: 'Students third post!' }
+    ]
+  })
 
+  const nathanTeacherPosts = await dbClient.post.createMany({
+    data: [
+      { userId: nathanTeacher.id, content: 'Teachers first post!' },
+      { userId: nathanTeacher.id, content: 'Teachers second post!' },
+      { userId: nathanTeacher.id, content: 'Teachers third post!' }
+    ]
+  })
 
-  //   const alicePost = await dbClient.post.create({
-  //     data: {
-  //       title: 'Why Python sucks',
-  //       content: 'Because I dont understand it...',
-  //       imageUrl: 'https://picsum.photos/200',
-  //       publishedAt: new Date(Date.now()),
-  //       comments: {
-  //         create: {
-  //           content: 'Python > JavaScript all day!',
-  //           user: { connect: { id: bob.id } }
-  //         }
-  //       },
-  //       categories: {
-  //         create: [{ name: 'Python' }, { name: 'JavaScript' }, { name: 'Coding' }]
-  //       },
-  //       user: { connect: { id: alice.id } }
-  //     }
-  //   })
+  const nathanStudentComment = await dbClient.comment.create({
+    data: {
+      content: 'Students first comment!',
+      user: {
+        connect: { id: nathanStudent.id }
+      },
+      post: {
+        connect: { id: 4 }
+      }
+    }
+  })
+
+  const nathanTeacherComment = await dbClient.comment.create({
+    data: {
+      content: 'Teachers first comment!',
+      user: {
+        connect: { id: nathanTeacher.id }
+      },
+      post: {
+        connect: { id: 1 }
+      }
+    }
+  })
+
+  const nathanTeacherCommentOnComment = await dbClient.comment.create({
+    data: {
+      content: 'Teachers first comment on a comment!',
+      user: {
+        connect: { id: nathanTeacher.id }
+      },
+      post: {
+        connect: { id: 1 }
+      },
+      child: {
+        connect: { id: 1 }
+      }
+    }
+  })
+
+  const createCohorts = await dbClient.cohort.createMany({
+    data: [{}, {}]
+  })
+
+  const connectStudentToCohort1 = await dbClient.user.update({
+    where: { id: nathanStudent.id },
+    data: {
+      cohort: {
+        connect: { id: 1 }
+      }
+    }
+  })
+
+  const connectTeacherToCohort1 = await dbClient.user.update({
+    where: { id: nathanTeacher.id },
+    data: {
+      cohort: {
+        connect: { id: 1 }
+      }
+    }
+  })
+
+  const deliveryLogForCohort1 = await dbClient.deliveryLog.create({
+    data: {
+      cohort: {
+        connect: { id: 1 }
+      },
+      user: {
+        connect: { id: nathanTeacher.id }
+      }
+    }
+  })
+
+  const createDeliveryLogLine = await dbClient.deliveryLogLine.create({
+    data: {
+      content: 'My delivery log line',
+      log: {
+        connect: { id: deliveryLogForCohort1.id }
+      }
+    }
+  })
 }
 main()
   .then(async () => {
