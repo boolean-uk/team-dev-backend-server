03649504ce465843ee1af7052cf3154780d41776
patrik <patrik.ellini@outlook.com>
2022-12-20 10:39:08 +0100

    added teacher verication access

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,12 +1,15 @@
 import { Router } from 'express'
 import { create, getAll } from '../controllers/post.js'
-import { validateAuthentication } from '../middleware/auth.js'
+import {
+  validateAuthentication,
+  validateTeacherRole
+} from '../middleware/auth.js'
 import { createComment, replyToComment } from '../controllers/comment.js'
 import { likePost, likeComment } from '../controllers/like.js'
 
 const router = Router()
 
-router.post('/', validateAuthentication, create)
+router.post('/', validateAuthentication, validateTeacherRole, create)
 router.get('/', validateAuthentication, getAll)
 router.post('/:postId/comments', validateAuthentication, createComment)
 router.post(
