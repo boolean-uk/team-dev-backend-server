6a18f4c1c7ef8a3c3bb55f1952a9123192abff1a
patrik <patrik.ellini@outlook.com>
2022-12-21 15:06:48 +0100

    added comment route

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,11 +1,7 @@
 import { Router } from 'express'
 import { create, getPost, getAll } from '../controllers/post.js'
 import { validateAuthentication } from '../middleware/auth.js'
-import {
-  getComment,
-  createComment,
-  replyToComment
-} from '../controllers/comment.js'
+import { createComment, replyToComment } from '../controllers/comment.js'
 import { likePost, likeComment } from '../controllers/like.js'
 
 const router = Router()
@@ -13,7 +9,6 @@ const router = Router()
 router.post('/', validateAuthentication, create)
 router.get('/', validateAuthentication, getAll)
 router.get('/:postId', validateAuthentication, getPost)
-router.get('/:postId/comments/:commentId', validateAuthentication, getComment)
 router.post('/:postId/comments', validateAuthentication, createComment)
 router.post(
   '/:postId/comments/:commentId',
