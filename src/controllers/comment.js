a372d8f69c385aaaa81310e88e01ed77d7c277d3
benji <benjipictor@gmail.com>
2023-04-14 15:55:58 +0100

    routing complete and create function built and working

---
+++ src/controllers/comment.js
@@ -0,0 +1,28 @@
+import { sendDataResponse } from '../utils/responses.js'
+import dbClient from '../utils/dbClient.js'
+
+export const createComment = async (req, res) => {
+    const { content } = req.body
+    const id = parseInt(req.params.id)
+    
+    if (!content) {
+        return sendDataResponse(res, 400, { error: 'Must provide content' })
+    }
+    const createdComment = await dbClient.comment.create({
+        data: {
+            content: content,
+            postId: id,
+            userId: req.user.id
+        }
+    })
+    return sendDataResponse(res, 201, {
+        comment: {
+            id: createdComment.id,
+            content: createdComment.content,
+            createdAt: createdComment.createdAt,
+            updatedAt: createdComment.updatedAt,
+            author: { ...req.user }
+        }
+    })
+}
+
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import { create, getAll } from '../controllers/post.js'
+import { createComment } from '../controllers/comment.js'
 import { validateAuthentication } from '../middleware/auth.js'
 
 const router = Router()
 
 router.post('/', validateAuthentication, create)
 router.get('/', validateAuthentication, getAll)
+
+router.post('/:id/comments', validateAuthentication, createComment)
 
 export default router
