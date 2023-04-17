8a738253c258063ee375b6f374fff27b7c42bc30
unknown <katdemey@outlook.com>
2023-04-17 17:23:21 +0100

    pre merge commit. Can update post on client side

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,5 +1,6 @@
 import { sendDataResponse } from '../utils/responses.js'
 import { createPost, getAllPosts } from '../domain/post.js'
+import dbClient from '../utils/dbClient.js'
 
 export const create = async (req, res) => {
   const { content } = req.body
@@ -48,4 +49,44 @@ export const getAll = async (req, res) => {
   })
 
   return sendDataResponse(res, 200, { posts: postsWithAuthor })
+}
+
+export const updateById = async (req, res) => {
+  const data = {}
+
+  const post = await dbClient.post.update({
+    where: {
+      id: Number(req.params.id)
+    },
+    data: {
+      content: req.body.content
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          role: true
+        }
+      }
+    }
+  })
+  // console.log(' req.body is the content', req.body)
+  // console.log(' req.params.id is the content', req.params.id)
+  // console.log(' after findUnique:', post)
+  // console.log(' post.userId after findUniquae:', post.userId)
+
+  if (req.user.role === 'TEACHER' || req.user.id === post.userId) {
+    // console.log('yes')
+    data.post = {
+      content: req.body.content,
+      updatedBy: req.user.id
+    }
+  } else {
+    return sendDataResponse(res, 403, {
+      authorization:
+        'You must be the original poster or a teacher to edit this post'
+    })
+  }
+
+  return sendDataResponse(res, 200, { post: data.post })
 }
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { create, getAll } from '../controllers/post.js'
 import { validateAuthentication } from '../middleware/auth.js'
-
+import { updateById } from '../controllers/post.js'
 const router = Router()
 
 router.post('/', validateAuthentication, create)
 router.get('/', validateAuthentication, getAll)
+
+router.patch('/:id', validateAuthentication, updateById)
 
 export default router
