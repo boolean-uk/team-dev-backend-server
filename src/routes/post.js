86c3d005cfdb241080226313a87dd8654fef36f6
unknown <katdemey@outlook.com>
2023-04-17 17:24:31 +0100

    pre merge request commit

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
-import { create, getAll } from '../controllers/post.js'
+import { create, getAll, updateById } from '../controllers/post.js'
 import { validateAuthentication } from '../middleware/auth.js'
-import { updateById } from '../controllers/post.js'
+
 const router = Router()
 
 router.post('/', validateAuthentication, create)
