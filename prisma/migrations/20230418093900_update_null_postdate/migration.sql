ea90bd9a2c10eee42f33b04560960de47636a441
MaybeFreak <michaelpope97@gmail.com>
2023-04-18 13:46:54 +0200

    Please just work

--- prisma/migrations/20230418093900_update_null_postdate/migration.sql
+++ prisma/migrations/20230418093900_update_null_postdate/migration.sql
@@ -1,3 +1,7 @@
 UPDATE "Post"
-SET "createdAt" = CURRENT_TIMESTAMP, "updatedAt" = CURRENT_TIMESTAMP
+SET "createdAt" = CURRENT_TIMESTAMP
 WHERE "createdAt" IS NULL
+
+UPDATE "Post"
+SET "updatedAt" = CURRENT_TIMESTAMP
+WHERE "updatedAt" IS NULL;
