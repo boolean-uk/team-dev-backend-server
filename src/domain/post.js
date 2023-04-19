a83a6e4687ae635da6de6bbfa098e69089ca7190
Asiyeyurtkuran <asiyeyurtkuran@gmail.com>
2023-04-19 17:41:34 +0100

    fixed conflicts

--- src/domain/post.js
+++ src/domain/post.js
@@ -23,3 +23,89 @@ export async function getAllPosts() {
     }
   })
 }
+
+export async function findById(id) {
+  return await dbClient.post.findUnique({
+    where: {
+      id: id
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          cohortId: true,
+          role: true,
+          profile: true
+        }
+      }
+    }
+  })
+}
+
+export async function createLike(userId, postId) {
+  return await dbClient.like.create({
+    data: {
+      user: {
+        connect: {
+          id: userId
+        }
+      },
+      post: {
+        connect: {
+          id: postId
+        }
+      }
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          cohortId: true,
+          role: true,
+          profile: true
+        }
+      },
+      post: {
+        select: {
+          id: true,
+          content: true,
+          user: {
+            select: {
+              id: true,
+              cohortId: true,
+              role: true,
+              profile: true
+            }
+          }
+        }
+      }
+    }
+  })
+}
+
+export async function deleteById(id) {
+  return await dbClient.post.delete({
+    where: {
+      id
+    }
+  })
+}
+
+export async function updatePostById(id, content) {
+  return await dbClient.post.update({
+    where: {
+      id
+    },
+    data: {
+      content
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          role: true
+        }
+      }
+    }
+  })
+}
