505283389cec706b4fef2b0b40c206c963d15451
MaybeFreak <michaelpope97@gmail.com>
2023-04-20 16:03:07 +0200

    Added model and migration for LikePost and LikeComment

---
+++ prisma/migrations/20230420140228_create_like_and_comment_model/migration.sql
@@ -0,0 +1,44 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post_Like` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post_Like" DROP CONSTRAINT "Post_Like_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Post_Like" DROP CONSTRAINT "Post_Like_userId_fkey";
+
+-- DropTable
+DROP TABLE "Post_Like";
+
+-- CreateTable
+CREATE TABLE "LikePost" (
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "postId" INTEGER NOT NULL,
+
+    CONSTRAINT "LikePost_pkey" PRIMARY KEY ("userId","postId")
+);
+
+-- CreateTable
+CREATE TABLE "LikeComment" (
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "commentId" INTEGER NOT NULL,
+
+    CONSTRAINT "LikeComment_pkey" PRIMARY KEY ("userId","commentId")
+);
+
+-- AddForeignKey
+ALTER TABLE "LikePost" ADD CONSTRAINT "LikePost_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "LikePost" ADD CONSTRAINT "LikePost_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "LikeComment" ADD CONSTRAINT "LikeComment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "LikeComment" ADD CONSTRAINT "LikeComment_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "Comment"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
