fe72e36cd5e66e8229037ffec0645c1ddb98a48f
MaybeFreak <michaelpope97@gmail.com>
2023-04-21 12:28:07 +0200

    Improved the data transfer SQL

--- prisma/migrations/20230420140632_transfer_data_into_like_post_and_comment_tables/migration.sql
+++ prisma/migrations/20230420140632_transfer_data_into_like_post_and_comment_tables/migration.sql
@@ -1,11 +1,11 @@
 -- Transfer data into the LikePost Table
-INSERT INTO "prisma"."LikePost" ("userId", "postId", "createdAt")
-    SELECT "userId", "postId", "createdAt" FROM "prisma"."Like"
+INSERT INTO "LikePost" ("userId", "postId", "createdAt")
+    SELECT "userId", "postId", "createdAt" FROM "Like"
     WHERE "postId" IS NOT NULL
     ON CONFLICT DO NOTHING;
 
 -- Transfer data into the LikeComment Table
-INSERT INTO "prisma"."LikeComment" ("userId", "commentId", "createdAt")
-    SELECT "userId", "commentId", "createdAt" FROM "prisma"."Like"
+INSERT INTO "LikeComment" ("userId", "commentId", "createdAt")
+    SELECT "userId", "commentId", "createdAt" FROM "Like"
     WHERE "commentId" IS NOT NULL
     ON CONFLICT DO NOTHING;
