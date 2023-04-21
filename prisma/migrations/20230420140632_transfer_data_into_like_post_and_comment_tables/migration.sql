ad306e9540801e6bb633943727dc4771a3d4892a
Oliver West <oliversimon.west@gmail.com>
2023-04-21 14:45:18 +0100

    revert to 9a5eaf991620768f0dbe245ad78ca1f5d91ebd0d

--- prisma/migrations/20230420140632_transfer_data_into_like_post_and_comment_tables/migration.sql
+++ prisma/migrations/20230420140632_transfer_data_into_like_post_and_comment_tables/migration.sql
@@ -1,11 +1,11 @@
 -- Transfer data into the LikePost Table
-INSERT INTO "LikePost" ("userId", "postId", "createdAt")
-    SELECT "userId", "postId", "createdAt" FROM "Like"
+INSERT INTO "prisma"."LikePost" ("userId", "postId", "createdAt")
+    SELECT "userId", "postId", "createdAt" FROM "prisma"."Like"
     WHERE "postId" IS NOT NULL
     ON CONFLICT DO NOTHING;
 
 -- Transfer data into the LikeComment Table
-INSERT INTO "LikeComment" ("userId", "commentId", "createdAt")
-    SELECT "userId", "commentId", "createdAt" FROM "Like"
+INSERT INTO "prisma"."LikeComment" ("userId", "commentId", "createdAt")
+    SELECT "userId", "commentId", "createdAt" FROM "prisma"."Like"
     WHERE "commentId" IS NOT NULL
     ON CONFLICT DO NOTHING;
