070d361fc6dd7106d5dc656635eac7440105ba5c
MaybeFreak <michaelpope97@gmail.com>
2023-04-21 12:14:55 +0200

    removed old likes schema

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,118 +1,147 @@
 import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcrypt'
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 async function seed() {
-    const cohort = await createCohort()
+  const cohort = await createCohort()
 
-    const student = await createUser('student@test.com', 'Testpassword1!', cohort.id, 'Joe', 'Bloggs', 'Hello, world!', 'student1')
-    const teacher = await createUser('teacher@test.com', 'Testpassword1!', null, 'Rick', 'Sanchez', 'Hello there!', 'teacher1', 'TEACHER')
+  const student = await createUser(
+    'student@test.com',
+    'Testpassword1!',
+    cohort.id,
+    'Joe',
+    'Bloggs',
+    'Hello, world!',
+    'student1'
+  )
+  const teacher = await createUser(
+    'teacher@test.com',
+    'Testpassword1!',
+    null,
+    'Rick',
+    'Sanchez',
+    'Hello there!',
+    'teacher1',
+    'TEACHER'
+  )
 
-    const post1 = await createPost(student.id, 'My first post!')
-    const post2 = await createPost(teacher.id, 'Hello, students')
-    const comment1 = await createComment(teacher.id, post1.id, 'congrats on the first post')
-    const comment2 = await createComment(student.id, post2.id, 'hello teacher')
-    await createLikeOnPost(student.id, post1.id)
-    await createLikeOnComment(student.id, comment1.id)
-    await createLikeOnComment(teacher.id, comment2.id)
-    process.exit(0);
+  const post1 = await createPost(student.id, 'My first post!')
+  const post2 = await createPost(teacher.id, 'Hello, students')
+  const comment1 = await createComment(
+    teacher.id,
+    post1.id,
+    'congrats on the first post'
+  )
+  const comment2 = await createComment(student.id, post2.id, 'hello teacher')
+  await createLikeOnPost(student.id, post1.id)
+  await createLikeOnComment(student.id, comment1.id)
+  await createLikeOnComment(teacher.id, comment2.id)
+  process.exit(0)
 }
 
 async function createLikeOnPost(userId, postId) {
-    const like = await prisma.like.create({
-        data: {
-            userId: userId,
-            postId: postId
-        }
-    })
+  const like = await prisma.likePost.create({
+    data: {
+      userId: userId,
+      postId: postId
+    }
+  })
 
-    console.info('post like created', like)
+  console.info('post like created', like)
 
-    return like
+  return like
 }
 
 async function createLikeOnComment(userId, commentId) {
-    const like = await prisma.like.create({
-        data: {
-            userId: userId,
-            commentId: commentId
-        }
-    })
+  const like = await prisma.likeComment.create({
+    data: {
+      userId: userId,
+      commentId: commentId
+    }
+  })
 
-    console.info('comment like created', like)
-    
-    return like
+  console.info('comment like created', like)
+
+  return like
 }
 
 async function createComment(userId, postId, content) {
-    const comment = await prisma.comment.create({
-        data: {
-            userId: userId,
-            postId: postId,
-            content: content
-        }
-    })
+  const comment = await prisma.comment.create({
+    data: {
+      userId: userId,
+      postId: postId,
+      content: content
+    }
+  })
 
-    console.info('comment created', comment)
+  console.info('comment created', comment)
 
-    return comment
+  return comment
 }
 
 async function createPost(userId, content) {
-    const post = await prisma.post.create({
-        data: {
-            userId,
-            content
-        },
-        include: {
-            user: true
-        }
-    })
+  const post = await prisma.post.create({
+    data: {
+      userId,
+      content
+    },
+    include: {
+      user: true
+    }
+  })
 
-    console.info('Post created', post)
+  console.info('Post created', post)
 
-    return post
+  return post
 }
 
 async function createCohort() {
-    const cohort = await prisma.cohort.create({
-        data: {}
-    })
+  const cohort = await prisma.cohort.create({
+    data: {}
+  })
 
-    console.info('Cohort created', cohort)
+  console.info('Cohort created', cohort)
 
-    return cohort
+  return cohort
 }
 
-async function createUser(email, password, cohortId, firstName, lastName, bio, githubUrl, role = 'STUDENT') {
-    const user = await prisma.user.create({
-        data: {
-            email,
-            password: await bcrypt.hash(password, 8),
-            role,
-            cohortId,
-            profile: {
-                create: {
-                    firstName,
-                    lastName,
-                    bio,
-                    githubUrl
-                }
-            }
-        },
-        include: {
-            profile: true
+async function createUser(
+  email,
+  password,
+  cohortId,
+  firstName,
+  lastName,
+  bio,
+  githubUrl,
+  role = 'STUDENT'
+) {
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password: await bcrypt.hash(password, 8),
+      role,
+      cohortId,
+      profile: {
+        create: {
+          firstName,
+          lastName,
+          bio,
+          githubUrl
         }
-    })
+      }
+    },
+    include: {
+      profile: true
+    }
+  })
 
-    console.info(`${role} created`, user)
+  console.info(`${role} created`, user)
 
-    return user
+  return user
 }
 
-seed()
-    .catch(async e => {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1)
-    })
+seed().catch(async (e) => {
+  console.error(e)
+  await prisma.$disconnect()
+  process.exit(1)
+})
