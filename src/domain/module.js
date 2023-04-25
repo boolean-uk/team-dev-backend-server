1d0d30cf5ce4351ecbdcc0e79d646139c40a6bbc
Oliver West <oliversimon.west@gmail.com>
2023-04-25 12:37:21 +0100

    included units in module getAll response

--- src/domain/module.js
+++ src/domain/module.js
@@ -1,7 +1,11 @@
 import dbClient from '../utils/dbClient.js'
 
 export async function getModules() {
-  return await dbClient.module.findMany()
+  return await dbClient.module.findMany({
+    include: {
+      units: true
+    }
+  })
 }
 
 export async function getModuleById(id) {
