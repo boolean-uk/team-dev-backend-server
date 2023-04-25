dd570b774e3a9e0ab102137ff90392056bbf5fba
Oliver West <oliversimon.west@gmail.com>
2023-04-25 12:40:51 +0100

    added cohorts and modules to response

--- src/domain/course.js
+++ src/domain/course.js
@@ -9,13 +9,22 @@ export async function createCourse(name) {
 }
 
 export async function getCourses() {
-  return await dbClient.course.findMany()
+  return await dbClient.course.findMany({
+    include: {
+      cohorts: true,
+      modules: true
+    }
+  })
 }
 
 export async function getCourseById(id) {
   return await dbClient.course.findUnique({
     where: {
       id: id
+    },
+    include: {
+      cohorts: true,
+      modules: true
     }
   })
 }
