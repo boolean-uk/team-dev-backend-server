a585edf63711f1db66e0b224b6504f5340ea3300
benji <benjipictor@gmail.com>
2023-04-25 16:04:29 +0100

    merge complete

--- src/routes/units.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { validateAuthentication } from '../middleware/auth.js'
-import { getAllByUnit } from '../controllers/exercises.js'
-const router = Router()
-
-router.get('/:id/exercises', validateAuthentication, getAllByUnit)
-
-export default router
