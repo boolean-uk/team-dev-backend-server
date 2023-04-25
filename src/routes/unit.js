ef861f9a6aa1043b1e19002eaaa555e328df8da4
unknown <katdemey@outlook.com>
2023-04-25 15:05:13 +0100

    pre merge commit

--- src/routes/unit.js
+++ src/routes/unit.js
@@ -1,12 +1,12 @@
 import { Router } from 'express'
 import { validateAuthentication } from '../middleware/auth.js'
 import { getUnitById, getUnitsByModule } from '../controllers/unit.js'
-
 const router = Router()
 
 // By id
 router.get('/:unitId', validateAuthentication, getUnitById)
 // Units from specific module
+// validate auth, check module exists, then get by units
 router.get('/modules/:moduleId/', validateAuthentication, getUnitsByModule)
 
 export default router
