b449b31f3be5d15d36db7765cdf684d3252ee224
rockettdev <kjr.dev@hotmail.com>
2023-07-21 14:19:03 +0100

    updated post creation, ensuring data is pushed to the DB

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,13 +1,25 @@
 import { sendDataResponse } from '../utils/responses.js'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 export const create = async (req, res) => {
   const { content } = req.body
+  const userId = req.user.id
 
   if (!content) {
     return sendDataResponse(res, 400, { content: 'Must provide content' })
   }
 
-  return sendDataResponse(res, 201, { post: { id: 1, content } })
+  const createdPost = await prisma.post.create({
+    data: {
+      content: content,
+      userId: userId
+    }
+  })
+
+  return sendDataResponse(res, 201, {
+    post: createdPost
+  })
 }
 
 export const getAll = async (req, res) => {
