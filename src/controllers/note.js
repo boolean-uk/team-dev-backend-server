9d114f9d94534e9b5969df9f0ec0f48b4ae7085b
NoroAxper <norikvardanyan.w@gmail.com>
2023-07-24 12:42:20 +0100

    adjusted code, such that notes can not be created about teachers

--- src/controllers/note.js
+++ src/controllers/note.js
@@ -1,12 +1,19 @@
 import { sendDataResponse } from '../utils/responses.js'
 import { createNote } from '../domain/note.js'
+import User from '../domain/user.js'
 
 export const create = async (req, res) => {
   const { content, userId } = req.body
+  const targetUser = await User._findByUnique('id', userId)
 
   if (!content) {
     return sendDataResponse(res, 400, {
       content: 'Please provide content in your notes immediatley!'
+    })
+  }
+  if (targetUser.role === 'TEACHER') {
+    return sendDataResponse(res, 400, {
+      content: 'Can not add notes on teachers.'
     })
   }
   try {
