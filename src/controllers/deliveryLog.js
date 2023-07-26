f340df6e5dafcb43bd3f675cd38771700cd309b0
carolarruda <carolinacalouroarruda@gmail.com>
2023-07-26 13:19:13 +0000

    apllied requested changes

--- src/controllers/deliveryLog.js
+++ src/controllers/deliveryLog.js
@@ -6,19 +6,18 @@ export const getLogs = async (req, res) => {
   const cohortId = parseInt(req.params.id)
   try {
     const foundCohort = await getCohort(cohortId)
-    if (foundCohort) {
-      const gettingLogs = await getDeliveryLog(cohortId)
-      if (gettingLogs.length === 0) {
-        return sendMessageResponse(
-          res,
-          404,
-          'No delivery logs found for this cohort'
-        )
-      }
-      return sendDataResponse(res, 201, gettingLogs)
-    } else {
+    if (!foundCohort) {
       return sendDataResponse(res, 404, 'Cohort not found')
     }
+    const gettingLogs = await getDeliveryLog(cohortId)
+    if (gettingLogs.length === 0) {
+      return sendMessageResponse(
+        res,
+        404,
+        'No delivery logs found for this cohort'
+      )
+    }
+    return sendDataResponse(res, 201, gettingLogs)
   } catch (error) {
     return sendMessageResponse(res, 500, 'Unable to get delivery logs')
   }
