288f29735865fbabe82ccf5f69a52e87f8895705
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-07-28 12:49:55 +0200

    implemented the module route

--- src/controllers/modules.js
+++ src/controllers/modules.js
@@ -1,12 +1,14 @@
-import { createModule } from '../domain/modules'
-import { sendDataResponse } from '../utils/responses'
+import { createModule } from '../domain/modules.js'
+import { sendDataResponse } from '../utils/responses.js'
 
 export const addModule = async (req, res) => {
   const { module } = req.body
 
   try {
-    createModule(module)
+    const resModule = await createModule(module)
+    return sendDataResponse(res, 201, { module: resModule })
   } catch (err) {
-    sendDataResponse(res, 400, { error: err.message })
+    // sendDataResponse(res, 400, { error: err.message })
+    console.log(err)
   }
 }
