8417387a957f47ba0d81464d059cb13ee75ccad9
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-08-01 19:10:54 +0200

    updated the routers and added the endpoint

--- src/routes/modules.js
+++ src/routes/modules.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { addModule, getAll } from '../controllers/modules.js'
+import { addModule, getAll, editModules } from '../controllers/modules.js'
 import {
   validateAuthentication,
   validateTeacherRole
@@ -8,4 +8,5 @@ import {
 const router = Router()
 router.post('/', validateAuthentication, validateTeacherRole, addModule)
 router.get('/:id', validateAuthentication, getAll)
+router.put('/:id', validateAuthentication, validateTeacherRole, editModules)
 export default router
