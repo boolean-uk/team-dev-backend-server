ed9792782ef9d7b8a5cfd2077339d16531d3c8ae
Your Name <your_email@address>
2023-08-02 11:06:28 +0100

    variables created for email interpolation

--- src/email/email.js
+++ src/email/email.js
@@ -1,14 +1,21 @@
 import sgMail from '@sendgrid/mail'
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-const msg = {
-  to: '',
+
+const recipient = ''
+const subject = ''
+const message = ''
+const hmtlInsert = ''
+
+const newEmail = {
+  to: `${recipient}`,
   from: 'cohort10.boolean@gmail.com',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>'
+  subject: `${subject}`,
+  text: `${message}`,
+  html: `${hmtlInsert}`
 }
+
 sgMail
-  .send(msg)
+  .send(newEmail)
   .then(() => {
     console.log('Email sent')
   })
