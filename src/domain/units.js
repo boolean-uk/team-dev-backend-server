5633a16a86d7b9c18d771db79e769a9388e8154c
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-08-02 18:20:50 +0200

    domain folder complete

--- src/domain/units.js
+++ src/domain/units.js
@@ -48,3 +48,20 @@ export async function createUnit(name, moduleId) {
     }
   })
 }
+
+export async function getUnitById(unitId, name) {
+  return await dbClient.unit.findUnique({
+    where: {
+      id: unitId,
+      Name: name
+    },
+    include: {
+      exercises: {
+        select: {
+          id: true,
+          name: true
+        }
+      }
+    }
+  })
+}
