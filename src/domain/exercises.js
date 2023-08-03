3df7b1406426dda89cafef085e3fb1f8f29c1672
NoroAxper <norikvardanyan.w@gmail.com>
2023-08-03 15:56:17 +0100

    able to create exercises for units

--- src/domain/exercises.js
+++ src/domain/exercises.js
@@ -1,4 +1,37 @@
 import dbClient from '../utils/dbClient.js'
+import { getUnitById } from './units.js'
+
+async function _findByUnique(name) {
+  const foundExercise = await dbClient.exercise.findUnique({
+    where: {
+      name
+    }
+  })
+  if (foundExercise) {
+    return foundExercise
+  }
+  return null
+}
+
+export async function findByExerciseName(name) {
+  return await _findByUnique(name)
+}
+export async function findByUnitId(unitId) {
+  return await getUnitById(unitId)
+}
+
+export async function createExercise(name, unitId) {
+  return await dbClient.exercise.create({
+    data: {
+      name,
+      unit: {
+        connect: {
+          id: unitId
+        }
+      }
+    }
+  })
+}
 
 export const getAll = async (unitId) => {
   return await dbClient.exercise.findMany({
