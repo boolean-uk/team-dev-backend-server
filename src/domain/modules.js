69fdb87083af20a1e66e5d19e93d9be77dbbc5c5
NoroAxper <norikvardanyan.w@gmail.com>
2023-08-03 11:36:30 +0100

    in modules.js/domain adjusted createModule to include courses data

--- src/domain/modules.js
+++ src/domain/modules.js
@@ -32,6 +32,14 @@ export async function createModule(name, courseId) {
           }
         }
       }
+    },
+    include: {
+      courses: {
+        select: {
+          id: true,
+          name: true
+        }
+      }
     }
   })
 }
