b2f39e98918fea7cb8ba7f7bfe7805488bbdb9d0
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-06 13:38:23 +0000

    fix: amend create()

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -3,11 +3,12 @@ import { sendDataResponse } from '../utils/responses.js'
 
 export const create = async (req, res) => {
   const { content } = req.body
+  const userId = req.user.id
 
   if (!content) {
     return sendDataResponse(res, 400, { content: 'Must provide content' })
   }
-  await createPost(content)
+  await createPost(content, userId)
   return sendDataResponse(res, 201, { post: { content } })
 }
 
