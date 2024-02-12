edcfc04c2846a600b7e30e2861a4fb3b000e8015
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-12 16:24:50 +0000

    chore: remove console.log

--- src/controllers/comment.js
+++ src/controllers/comment.js
@@ -2,7 +2,6 @@ import Comment from '../domain/comment.js'
 import { sendDataResponse } from '../utils/responses.js'
 
 export const getComments = async (req, res) => {
-  console.log('ran')
   const comments = await Comment.getAll()
   return sendDataResponse(res, 200, { comments })
 }
