885b6e5f099ac2864d01749d4caa02272664bc69
Kye <NovusAa12@proton.me>
2024-02-16 12:41:52 +0000

    add getStudentsByCohortId controller

--- src/controllers/cohort.js
+++ src/controllers/cohort.js
@@ -1,4 +1,5 @@
 import { createCohort, Cohort } from '../domain/cohort.js'
+import { Student } from '../domain/student.js'
 import { sendDataResponse, sendMessageResponse } from '../utils/responses.js'
 
 export const create = async (req, res) => {
@@ -18,5 +19,30 @@ export const getCohorts = async (req, res) => {
   } catch (e) {
     console.log('Error retrieving cohorts', e)
     return sendMessageResponse(res, 500, 'Unable to get the list of cohorts')
+  }
+}
+
+export const getStudentsByCohortId = async (req, res) => {
+  const cohortId = parseInt(req.params.id)
+
+  if (!cohortId) {
+    return sendMessageResponse(res, 400, { error: 'No provided cohort ID' })
+  }
+
+  try {
+    const foundCohort = await Cohort.getAll()
+
+    const hasCohort = foundCohort.find((cohort) => cohort.id.id === cohortId)
+
+    if (!hasCohort) {
+      return sendMessageResponse(res, 404, 'No cohort found with provided ID')
+    }
+
+    const foundStudents = await Student.getAllStudentsByCohortId(cohortId)
+
+    return res.status(200).send({ students: foundStudents })
+  } catch (e) {
+    console.log(e)
+    return sendMessageResponse(res, 500, 'Unable to get students')
   }
 }
