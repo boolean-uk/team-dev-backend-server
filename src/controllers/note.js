5250fbb724d17f129274504f34a49df5f64b3355
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-02-21 16:00:28 +0000

    feat: implement createNote controller

---
+++ src/controllers/note.js
@@ -0,0 +1,41 @@
+import Note from '../domain/note.js'
+import Student from '../domain/student.js'
+import Teacher from '../domain/teachers.js'
+
+import { sendDataResponse } from '../utils/responses.js'
+
+export const createNote = async (req, res) => {
+  const { title, content, studentUserId, teacherUserId } = req.body
+
+  try {
+    await Student.findByUserId(studentUserId)
+  } catch (error) {
+    return sendDataResponse(res, 404, {
+      error: 'Student not found',
+      status: 404
+    })
+  }
+
+  try {
+    await Teacher.findByUserId(teacherUserId)
+  } catch (error) {
+    return sendDataResponse(res, 404, {
+      error: 'Teacher not found',
+      status: 404
+    })
+  }
+
+  try {
+    const createdNote = await Note.create(
+      title,
+      content,
+      studentUserId,
+      teacherUserId
+    )
+
+    return sendDataResponse(res, 201, createdNote)
+  } catch (error) {
+    console.error('Error creating note:', error)
+    return sendDataResponse(res, 500, { error: 'Something went wrong' })
+  }
+}
